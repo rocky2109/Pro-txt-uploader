99f0372f32742ba4c8e0427de86f2d09f6c57711
sonic kuwal <158008154+Soninkuwal@users.noreply.github.com>
2025-04-18 09:02:03 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,15 @@
 # Don't Remove Credit Tg - @Tushar0125
 # Ask Doubt on telegram @Tushar0125
 
+# first comend
+# git clone https://github.com/Soninkuwal/ADVANCE-TXT-UPLOADER
+# cd text-leech-bot
+
+# senad comend
+# sudo apt install docker.io -y
+# sudo docker build -t text-leech-bot-by-ashu .
+# sudo docker run -it --rm text-leech-bot-by-ashu
+
 from os import environ
 
 API_ID = int(environ.get("API_ID", "")) #Replace with your api id
